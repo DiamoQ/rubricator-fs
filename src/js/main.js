8a74cb9886341e15fab8897102a2b57894301061
diamond220qwert@mail.ru <diamond220qwert@mail.ru>
2024-07-23 09:45:54 +0300

    Правки в навигации, открытии окна навигации, стилях

--- src/js/main.js
+++ src/js/main.js
@@ -30,14 +30,17 @@ textarea?.addEventListener('keyup', function(){
 const hamburger = document.querySelector('.header-hamburger__wrapper');
       navigationMenu = document.querySelector(".header-wrapper");
       header = document.querySelector(".navigation");
-
+      body = document.querySelector("body");
+      
 window.addEventListener("click", e => {
   if (e.target.closest(".header-hamburger__wrapper")) {
     navigationMenu.classList.add("active");
     header.classList.add("active");
+    body.style.cssText = 'overflow-y: hidden;';
     header.classList.contains("notactive") && header.classList.remove("notactive");
   } else if (e.target != header && !e.target.classList.contains("navigation__section") && !e.target.classList.contains("navigation__input") && !e.target.classList.contains("navigation___item") && !e.target.classList.contains("navigation__list") && !e.target.classList.contains("header-wrapper") ) {
     navigationMenu.classList.remove("active");
+    body.style.cssText = 'overflow-y: auto;';
     header.classList.contains("active") && header.classList.add("notactive");
     header.classList.remove("active");
   }
